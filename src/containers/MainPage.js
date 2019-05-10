0358d8c112f69db208b81a7e8652e60a354a9557
Dusty211 <kylebenz@hotmail.com>
2019-05-10 14:31:36 -0400

    basic tidying up of default files and config.

---
+++ src/containers/MainPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MainPage extends Component {
+  render() {
+    return(
+      <div id="mainpage">
+      "This is the main page"
+      </div>
+    )
+  }
+}
+
+export default MainPage;
