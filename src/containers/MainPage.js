ab6437dad4da6eda65f040ef9315fb6565a9c52a
Dusty211 <kylebenz@hotmail.com>
2019-05-15 17:42:34 -0400

    basic setup and modularization of selector component.

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import PlatformSelector from '../components/PlatformSelector.js'
 
 class MainPage extends Component {
   render() {
     return(
       <div id="mainpage">
       "This is the main page"
+      <PlatformSelector
+      forLabel={'departure-station-selector'}
+      visibleLabel ={'From Station'}/>
       </div>
     )
   }
