e8db2866b24ad32435d1f7e809d5530b8a0d40a6
Dusty211 <kylebenz@hotmail.com>
2019-05-26 22:52:04 -0400

    Added material-ui table to arrivals, and added padding to arrivals table and mainpage component.

--- src/components/Arrivals.js
+++ src/components/Arrivals.js
@@ -3,9 +3,33 @@ import React, { Component } from 'react';
 //Redux:
 import { connect } from 'react-redux';
 
+//material-ui
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import LensIcon from "@material-ui/icons/Lens";
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 1,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 200
+  }
+});
+
 class Arrivals extends Component {
 
   render() {
+
+    const { classes } = this.props;
 
     const arrivals = () => {
       if (this.props.currentArrivals.arrivals) {
@@ -15,19 +39,57 @@ class Arrivals extends Component {
       }
     }
 
+    let id = 0;
+
+    const createData = (line, minutes, cars, destination) => {
+      id += 1;
+      return { id, line, minutes, cars, destination };
+    }
+
+    const rows = arrivals().map(arrival => {
+      return createData(arrival.line, arrival.minutes, arrival.cars, arrival.destination)
+    })
+
+    const tableRowStyle = { height: "25px", paddingLeft: 15, paddingRight: 15};
+    const tableHeaderStyle = { height: "25px", paddingLeft: 15, paddingRight: 15};
+
     return(
+
       <div>
-      <br/>Arrivals:<br/>
-      Minutes - Line - Cars - Destination<br/>
-      <ul>
-      {arrivals().map(arrival => {
-        return(<li key={arrival.id}>{`${arrival.minutes} min. -- ${arrival.line} -- ${arrival.cars} car -- ${arrival.destination}`}</li>)
-      })}
-      </ul>
-      </div>
+        <br/>
+        Arrivals:
+        <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow style={tableHeaderStyle}>
+              <TableCell style={tableHeaderStyle}>Line</TableCell>
+              <TableCell style={tableHeaderStyle} align="right">Min.</TableCell>
+              <TableCell style={tableHeaderStyle} align="right">Cars</TableCell>
+              <TableCell style={tableHeaderStyle} align="right">Destination</TableCell>
+
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow style={tableRowStyle} key={row.id}>
+                <TableCell style={tableRowStyle} component="th" scope="row"><LensIcon style={{color: `${row.line}`}}/></TableCell>
+                <TableCell style={tableRowStyle} align="right">{row.minutes}</TableCell>
+                <TableCell style={tableRowStyle} align="right">{row.cars}</TableCell>
+                <TableCell style={tableRowStyle} align="right">{row.destination}</TableCell>
+
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
     )
   }
 }
+
+Arrivals.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -35,4 +97,4 @@ const mapStateToProps = (state) => {
   };
 }
 
-export default connect(mapStateToProps, null)(Arrivals)
+export default connect(mapStateToProps, null)(withStyles(styles)(Arrivals));
